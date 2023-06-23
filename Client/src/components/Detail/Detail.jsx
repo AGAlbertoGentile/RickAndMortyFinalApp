c7920fe4334a71074c9fc378e637a389b44b5c01
albertog1 <alberto.gentile1@gmail.com>
2023-06-23 20:11:31 -0300

    Update detail and form

--- Client/src/components/Detail/Detail.jsx
+++ Client/src/components/Detail/Detail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from 'axios';
+import style from './detail.module.css'
 
 
 export default function Detail(){
@@ -21,13 +22,17 @@ export default function Detail(){
     }, [id]);
 
     return(
-        <div>
-            <h2>{character.name}</h2>
-            <h2>{character.status}</h2>
-            <h2>{character.species}</h2>
-            <h2>{character.gender}</h2>
+        <div className={style.detailContainer}>
+            <div className={style.descriptionText}>
+                <h2>{character.name}</h2>
+                <h2>{character.status}</h2>
+                <h2>{character.species}</h2>
+                <h2>{character.gender}</h2>
+            </div>
             {/* <h2>{character.origin?.name}</h2> */}
-            <img src={character.image} alt={character.name} />
+            <div>
+                <img className={style.imgCharacter} src={character.image} alt={character.name} />
+            </div>
         </div>
     )
 }
--- Client/src/components/Form/Form.jsx
+++ Client/src/components/Form/Form.jsx
@@ -1,90 +1,99 @@
 import React from "react";
 import { useState } from "react";
 import style from "./form.module.css";
+// import backgroundLog from '../../assets/rick-and-morty-watching-tv.mp4'
+// import backgroundLog from '../../assets/the-throne-rick-and-orty-wallpaper.mp4'
+import backgroundLog from '../../assets/rick-4.mp4'
 
 
-export function validate(userData){
 
-    const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+export function validate(userData) {
 
-    let errors = {};
-  
-    if (!regexEmail.test(userData.email)) {
-        errors.email = 'Debe ser un correo electrónico válido';
-      }
-    
-      if (!userData.email) {
-        errors.email = 'El campo de correo electrónico no puede estar vacío';
-      }
-    
-      if (userData.email.length > 35) {
-        errors.email = 'El correo electrónico no puede tener más de 35 caracteres';
-      }
-    
-      if (!/\d/.test(userData.password)) {
-        errors.password = 'La contraseña debe contener al menos un número';
-      }
-    
-      if (userData.password.length < 6 || userData.password.length > 10) {
-        errors.password = 'La contraseña debe tener una longitud entre 6 y 10 caracteres';
-      }
-    
-      return errors;
-    }
-    
+  const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-export function Form(props){
-    
-    const {login} = props;
+  let errors = {};
 
-    const [userData, setUserData] = useState({
-        email: "",
-        password:"",
+  if (!regexEmail.test(userData.email)) {
+    errors.email = 'Debe ser un correo electrónico válido';
+  }
+
+  if (!userData.email) {
+    errors.email = 'El campo de correo electrónico no puede estar vacío';
+  }
+
+  if (userData.email.length > 35) {
+    errors.email = 'El correo electrónico no puede tener más de 35 caracteres';
+  }
+
+  if (!/\d/.test(userData.password)) {
+    errors.password = 'La contraseña debe contener al menos un número';
+  }
+
+  if (userData.password.length < 6 || userData.password.length > 10) {
+    errors.password = 'La contraseña debe tener una longitud entre 6 y 10 caracteres';
+  }
+
+  return errors;
+}
+
+
+export function Form(props) {
+
+  const { login } = props;
+
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    login(userData);
+  };
+
+
+  function handleChange(event) {
+    setUserData({
+      ...userData,
+      [event.target.name]: event.target.value
     });
-
-    const [errors, setErrors] = useState({});
-
-    function handleSubmit(event){
-      event.preventDefault();
-      login(userData);
-    };
-
-
-    function handleChange(event){
-      setUserData({
+    setErrors(
+      validate({
         ...userData,
         [event.target.name]: event.target.value
-      });
-      setErrors(
-            validate({
-            ...userData,
-            [event.target.name]: event.target.value
-          }));
-    }
-    
-    return(
-        <div>
-            <form className={style.container} onSubmit={handleSubmit}>
-                <label className={style.title}>Email</label>
-                <input 
-                  name="email" 
-                  placeholder={"Escribe tu email"} 
-                  type="text" 
-                  value={userData.email} 
-                  onChange={handleChange}/>
-                  <p className='danger'>{errors.email}</p>  
-                <label className={style.title}>Password</label>
-                <input  
-                  name="password" 
-                  placeholder={"Password"} 
-                  type="password" 
-                  value={userData.password} 
-                  onChange={handleChange}/>
-                  <p className='danger'>{errors.message}</p>    
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
+      }));
+  }
+
+  return (
+    <div>
+      <div className={style.globalContainer}>
+        <video autoPlay loop muted playbackRate={0.5} id="videoFondo">
+          <source src={backgroundLog} type="video/mp4"/>
+        </video>
+      </div>
+      <form className={style.container} onSubmit={handleSubmit}>
+        <label className={style.title}>Email</label>
+        <input className={style.placeholderLogin}
+          name="email"
+          placeholder={"Escribe tu email"}
+          type="text"
+          value={userData.email}
+          onChange={handleChange} />
+        <p className='danger'>{errors.email}</p>
+        <label className={style.title}>Password</label>
+        <input
+          name="password"
+          placeholder={"Password"}
+          type="password"
+          value={userData.password}
+          onChange={handleChange} />
+        <p className='danger'>{errors.message}</p>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 
